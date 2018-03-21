62b66122d77a84c95f6841f9076f4e2fc388bf27
albertogk <agk1esoc@gmail.com>
2018-03-21 09:20:36 +0100

    Testers

---
+++ Pr3/src/testers/testerPelicula.java
@@ -0,0 +1,13 @@
+package testers;
+import cine.*;
+
+public class testerPelicula {
+
+	public static void main(String[] args) {
+		Pelicula p = new Pelicula("La forma del Agua", "Alex de la Iglesia", 2018, "Increible pelicula", Genero.DRAMA, 120);
+		System.out.println(p);
+		
+
+	}
+
+}
---
+++ Pr3/src/testers/testerSala.java
@@ -0,0 +1,27 @@
+package testers;
+import cine.*;
+
+public class testerSala {
+
+	public static void main(String[] args) {
+		
+		Sala s1 = new Sala(20);
+		Sala s2 = new Sala(50);
+		
+		System.out.println(s1);
+		System.out.println(s2);
+				
+		Pelicula p = new Pelicula("La forma del Agua", "Alex de la Iglesia", 2018, "Increible pelicula", Genero.DRAMA, 120);
+		
+		Sesion ses1 = new Sesion(2018, 12, 12, 18, 30, p, s1);
+		Sesion ses2 = new Sesion(2018, 12, 12, 19, 00, p, s1);
+		Sesion ses3 = new Sesion(2018, 11, 12, 19, 00, p, s2);
+		Sesion ses4 = new Sesion(2018, 12, 12, 22, 00, p, s1);
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		
+
+	}
+
+}
