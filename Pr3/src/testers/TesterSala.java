cc88dbf531529bf173d18c82590a24e02396b679
Miguel Arconada WIN <miguelarman@gmail.com>
2018-03-22 11:01:40 +0100

    testersala comentado

--- Pr3/src/testers/TesterSala.java
+++ Pr3/src/testers/TesterSala.java
@@ -1,26 +1,58 @@
 package testers;
+
 import cine.*;
 
 public class TesterSala {
 
 	public static void main(String[] args) {
-		
+
+		Boolean check;
+
+		// Creamos dos salas
 		Sala s1 = new Sala(20);
 		Sala s2 = new Sala(50);
-		
+
+		// Imprimimos la informacion de las dos salas
 		System.out.println(s1);
 		System.out.println(s2);
-				
-		Pelicula p = new Pelicula("La forma del Agua", "Alex de la Iglesia", 2018, "Increible pelicula", Genero.DRAMA, 120);
-		
-		new Sesion(2018, 12, 12, 18, 30, p, s1);
-		new Sesion(2018, 12, 12, 19, 00, p, s1);
-		new Sesion(2018, 11, 12, 19, 00, p, s2);
-		new Sesion(2018, 12, 12, 22, 00, p, s1);
-		
+
+		Pelicula p = new Pelicula("La forma del Agua", "Alex de la Iglesia", 2018, "Increible pelicula", Genero.DRAMA,
+				120);
+
+		// Creamos sesiones en estas salas
+		Sesion sesion1 = new Sesion(2018, 12, 12, 18, 30, p, s1);
+		Sesion sesion2 = new Sesion(2018, 12, 12, 19, 00, p, s1);
+		Sesion sesion3 = new Sesion(2018, 11, 12, 19, 00, p, s2);
+		Sesion sesion4 = new Sesion(2018, 12, 12, 22, 00, p, s1);
+
+		/*
+		 * Volvemos a imprimir la informacion de las salas para comprobar que se
+		 * actualiza correctamente
+		 */
 		System.out.println(s1);
 		System.out.println(s2);
+
+		// Borramos una sesion de la sala
+		check = s1.removeSesion(sesion1);
+		if (!check) {
+			System.out.println("No se ha podido eliminar la sesion correctamente");
+		}
+
+		// Imprimimos las salas para comprobar que se eliminan las sesiones
+		System.out.println(s1);
+		System.out.println(s2);
+
 		
+		
+		// Intentamos borrar una sesion que no esta en la sala
+		check = s2.removeSesion(sesion4);
+		if (!check) {
+			System.out.println("No se ha podido eliminar la sesion correctamente");
+		}
+
+		// Imprimimos las salas para comprobar que no se ha eliminado nada
+		System.out.println(s1);
+		System.out.println(s2);
 
 	}
 
