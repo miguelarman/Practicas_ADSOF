979c37c8bc3bff6f6ba2a165292edefc2b7d8adf
Miguel Arconada WIN <miguelarman@gmail.com>
2018-03-22 10:54:16 +0100

    comentado el tester de sala

--- Pr3/src/testers/TesterSesion.java
+++ Pr3/src/testers/TesterSesion.java
@@ -7,8 +7,14 @@ public class TesterSesion {
 		Sala s = new Sala(1);
 		Boolean check;
 		
+		// Generamos una pelicula
 		Pelicula p = new Pelicula("La forma del Agua", "Alex de la Iglesia", 2018, "Increible pelicula", Genero.DRAMA, 120);
 		
+		
+		/* 
+		 * Intentamos crear 4 sesiones para esa pelicula. Aqui comprobamos
+		 * que no se pueden solapar sesiones (s2 se solaparia a s1 y s4 a s3 
+		 */
 		Sesion s1 = new Sesion(2018, 12, 12, 18, 30, p, s);
 		if (s1.getSala() == null) {
 			System.out.println("No se ha podido crear la sesion correctamente");
@@ -26,25 +32,40 @@ public class TesterSesion {
 			System.out.println("No se ha podido crear la sesion correctamente");
 		}
 		
+		
+		/*
+		 * Imprimimos las salas que se han creado
+		 */
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(s3);
 		System.out.println(s4);
 		
+		/*
+		 * Actualizamos las butacas vendidas, es decir, simulamos que se vende
+		 * una, y como la sala la hemos creado con una sola butaca comprobamos
+		 * que devuelve true
+		 */
 		check = s1.actualizarButacasVendidas();
 		if(check == false) {
 			System.out.println("No hay más butacas.\n");
 		}
 		
+		// Imprimimos la sala para comprobar que se ha actualizado
 		System.out.println(s1);
 		
+		/*
+		 * Actualizamos las butacas vendidas, es decir, simulamos que se vende
+		 * otra, y como la sala la hemos creado con una sola butaca y ya hemos
+		 * disminuido las butacas disponibles comprobamos que devuelve false
+		 */
 		check = s1.actualizarButacasVendidas();
 		if(check == false) {
 			System.out.println("No hay más butacas.\n");
 		}
 		
+		// Volvemos a imprimir la sesion para comprobar que se actualiza correctamente
 		System.out.println(s1);
 
 	}
-
 }
