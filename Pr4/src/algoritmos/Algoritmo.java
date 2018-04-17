fe4daaa5232cb027023d7c04d4961a91835b0c54
Miguel Arconada WIN <miguelarman@gmail.com>
2018-04-17 18:05:10 +0200

    aouhsd

--- Pr4/src/algoritmos/Algoritmo.java
+++ Pr4/src/algoritmos/Algoritmo.java
@@ -1,6 +1,7 @@
 package algoritmos;
 
 import java.util.ArrayList;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
--- Pr4/src/algoritmos/OrganizadorPorFitness.java
+++ Pr4/src/algoritmos/OrganizadorPorFitness.java
@@ -4,6 +4,8 @@ import java.util.Comparator;
 
 import interfaces.IIndividuo;
 
+
+
 public class OrganizadorPorFitness implements Comparator<IIndividuo> {
 
 	@Override
