5ac8c1f141a9236098d2477d13ecc4a355c266c4
albertogk <agk1esoc@gmail.com>
2018-04-17 17:00:50 +0200

    El nazi perfecto

--- Pr4/src/otros/TesterSwap.java
+++ Pr4/src/otros/TesterSwap.java
@@ -27,7 +27,7 @@ public class TesterSwap {
 //		DominioAritmetico.setMargen(0.5);
 		
 		try {
-			Algoritmo alg = new Algoritmo(5, 150, 0.9, 10000, 6);
+			Algoritmo alg = new Algoritmo(5, 200, 0.9, 100000, 4);
 			alg.ejecutar(d);
 		} catch (ArgumentosInvalidosAlgoritmo e) {
 			// TODO Auto-generated catch block
