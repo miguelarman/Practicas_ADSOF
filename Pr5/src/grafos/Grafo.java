7e990f884b6056fada871596646515c1bd47e55d
Miguel Arconada WIN <miguelarman@gmail.com>
2018-04-27 17:25:00 +0200

    aoisd

---
+++ Pr5/src/grafos/Grafo.java
@@ -0,0 +1,51 @@
+package grafos;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class Grafo<T> {
+	protected Map<Integer, Vertice<T>> vertices;
+
+	public Vertice<T> addVertice(T datos) {
+		// TODO
+
+		return null;
+	}
+
+	protected Vertice<T> addVertice(int id, T datos) {
+		// TODO
+		return null;
+	}
+
+	public List<Vertice<T>> getVertices() {
+		// TODO
+		return null;
+	}
+
+	public int getNumVertices() {
+
+		return -1;
+	}
+
+	public abstract void addArco(Vertice<T> v1, Vertice<T> v2, double peso);
+
+	public boolean existeArco(Vertice<T> v1, Vertice<T> v2) {
+		// TODO
+		return false;
+	}
+
+	public int getNumArcos() {
+		// TODO
+		return -1;
+	}
+
+	public abstract double getPesoDe(Vertice<T> v1, Vertice<T> v2);
+
+	public abstract List<Vertice<T>> getVecinosDe(Vertice<T> v); // devuelve los vértices que tienen un arco con v (en
+																	// grafo dirigido, v ha de ser origen de los arcos)
+
+	public String toString() { // los vértices del grafo han de presentarse ORDENADOS POR IDENTIFICADOR
+		// TODO
+		return null;
+	}
+}
---
+++ Pr5/src/grafos/GrafoNoDirigido.java
@@ -0,0 +1,25 @@
+package grafos;
+
+import java.util.List;
+
+public class GrafoNoDirigido<T> extends Grafo<T> {
+
+	@Override
+	public void addArco(Vertice<T> v1, Vertice<T> v2, double peso) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getPesoDe(Vertice<T> v1, Vertice<T> v2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Vertice<T>> getVecinosDe(Vertice<T> v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
